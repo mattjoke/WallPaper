3926e337a2f6b1eb331dd2f65ad069a0a64dac62
Matej Hakoš <matejhakos@gmail.com>
2020-07-22 15:49:32 +0200

    added tripple swipe

--- Card.js
+++ Card.js
@@ -1,7 +1,62 @@
-class Card {
-    constructor() {
-        console.log("HEY");
+class card {
+    constructor(id) {
+        this.id = id;
+        this.createDiv();
+        this.myself = $('#' + this.id);
+        this.parent = this.myself.parent();
+        this.x = Math.floor(Math.random() * (this.parent.width() - 500));
+        this.y = Math.floor(Math.random() * (this.parent.height() - 250));
+        this.drawDiv();
+        this.man = undefined;
+        this.scale = 1;
+
+        this.initGestures();
+    }
+    drawDiv = () => {
+        $(this.myself).css({
+            'top': this.y + 'px',
+            'left': this.x + 'px'
+        });
+    }
+    createDiv = () => {
+        let newDiv = document.createElement('div');
+        newDiv.id = this.id;
+        newDiv.className = 'item';
+        newDiv.textContent = this.id + 1;
+        document.getElementById('canvas').appendChild(newDiv);
+    }
+
+    initGestures = () => {
+        let element = document.getElementById(this.id);
+
+        this.man = new Hammer.Manager(element);
+        let pinch = new Hammer.Pinch();
+        let pan = new Hammer.Pan();
+
+        pan.requireFailure(pinch);
+
+        this.man.add([pan, pinch]);
+
+        let panHandler = (ev) => {
+            this.myself.css({
+                'top': ev.gesture.center.y + 'px',
+                'left': (ev.gesture.center.x - 50) + 'px'
+            });
+        }
+
+        let pinchHandeler = (ev) => {
+            const scale = ev.gesture.scale * 1.2;
+            this.scale = scale;
+            this.myself.css({
+                '-webkit-transform': 'scale(' + scale + ')',
+                '-moz-transform': 'scale(' + scale + ')',
+                '-ms-transform': 'scale(' + scale + ')',
+                '-o-transform': 'scale(' + scale + ')',
+                'transform': 'scale(' + scale + ')'
+            });
+        }
+
+        this.myself.hammer().bind("pinch", pinchHandeler);
+        this.myself.hammer().bind("pan", panHandler);
     }
 }
-
-export default Card;
--- script.js
+++ script.js
@@ -1,78 +1,16 @@
 let canvas = document.getElementById("canvas");
 
-let spawnSquares = (id) => {
-    let newDiv = document.createElement('div');
-    newDiv.id = id;
-    newDiv.className = 'item';
-    newDiv.textContent = id + 1;
-    canvas.appendChild(newDiv);
-}
-
-let randomPosition = (id) => {
-    let parent = $(id).parent();
-    let x = Math.floor(Math.random() * (parent.width() - 500));
-    let y = Math.floor(Math.random() * (parent.height() - 250));
-    $(id).css({
-        'position': 'absolute',
-        'top': y + 'px',
-        'left': x + 'px'
-    });
-}
-
-
+let array = [];
 for (let i = 0; i < 5; i++) {
-    spawnSquares(i);
-    randomPosition('#' + i);
+    let c = new card(i);
+    array.push(c);
 }
 
-let squares = document.getElementsByClassName('item');
-
-let pinch = new Hammer.Pinch();
-let rotate = new Hammer.Pinch();
-
-let arr = [];
+let hammertime = new Hammer(canvas);
 
 
-$('.item').each((id) => {
-    let element = document.getElementById(id);
-    arr[id] = new Hammer(element);
-    arr[id].add([pinch, rotate]);
-    pinch.recognizeWith(rotate);
+hammertime.add(new Hammer.Swipe({ event: 'trippleswipe', pointers: 3 }));
 
-    arr[id].on("pan", (ev) => {
-        $(element).css({
-            'top': ev.center.y + 'px',
-            'left': (ev.center.x - 50) + 'px'
-        });
-    });
-
-    arr[id].on('rotate', (ev) => {
-        console.log(ev.rotation)
-
-        $(element).css({
-            '-webkit-transform': 'rotation(' + ev.rotation + 'deg);',  /* Chrome, Safari 3.1+ */
-            '-moz-transform': 'rotation(' + ev.rotation + 'deg); scale(' + ev.scale + ')',  /* Firefox 3.5-15 */
-            '-ms-transform': 'rotation(' + ev.rotation + 'deg); scale(' + ev.scale + ')',  /* IE 9 */
-            '-o-transform': 'rotation(' + ev.rotation + 'deg); scale(' + ev.scale + ')',  /* Opera 10.50-12.00 */
-            'transform': 'rotation(45deg);'  /* Firefox 16+, IE 10+, Opera 12.10+ */
-        })
-    });
-
-    arr[id].on('pinch', (ev) => {
-        let scale = ev.scale * 2;
-
-        console.log(id);
-
-        $('#' + id).css({
-            '-webkit-transform': 'scale(' + scale + ')',  /* Chrome, Safari 3.1+ */
-            '-moz-transform': 'scale(' + scale + ')',  /* Firefox 3.5-15 */
-            '-ms-transform': 'scale(' + scale + ')',  /* IE 9 */
-            '-o-transform': 'scale(' + scale + ')',  /* Opera 10.50-12.00 */
-            'transform': 'scale(' + scale + ')'  /* Firefox 16+, IE 10+, Opera 12.10+ */
-        })
-    });
+hammertime.on('trippleswipe', (ev) => {
+    console.log("Tripple swiper;")
 });
-
-let getArr = () => {
-    console.table(arr);
-}
